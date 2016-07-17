34989acd1c8e6e6780b29e81e74d55aab1a56f15
Kenny Chen <kenny1413@gmail.com>
2016-07-17 13:06:30 -0400

    Initial Commit

---
+++ PokeWalker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PokeWalker
+//
+//  Created by kenny on 7/17/16.
+//  Copyright © 2016 kenny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PokeWalker/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PokeWalker
+//
+//  Created by kenny on 7/17/16.
+//  Copyright © 2016 kenny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
